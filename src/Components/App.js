8894c8613313b2a77605db57c65160415ce53744
Suresh Murali <m.suresh.fx@gmail.com>
2017-10-12 20:39:01 +0900

    This is where it all begins
    Portfolio project's initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../Styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Portfolio</h1>
+        </header>
+        <p className="App-intro">
+          Web app to advertise yourself
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
