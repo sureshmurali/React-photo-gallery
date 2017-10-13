f9152f1b51002fd919778f464557b62349a26daa
Suresh Murali <m.suresh.fx@gmail.com>
2017-10-13 11:37:43 +0900

    Flexbox grid. Hero image and title completed.

---
+++ src/Components/Hero.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import '../Styles/Hero.css';
+import {Row, Col} from 'react-flexbox-grid';
+
+class Hero extends Component {
+  render() {
+    return (
+      <Row className="FullScreenHeight">
+        <Col xs={12} sm={12} md={6} className="Menu">
+          <div className="UserNameStyle">Melissa</div>
+        </Col>
+        <Col xs={12} sm={12} md={6}>
+          <div className="HeroImage"></div>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Hero;
