8989b46d17a06dafd465077c0f059d2214c04f71
Suresh Murali <m.suresh.fx@gmail.com>
2017-10-13 15:11:15 +0900

    Gradient candy buttons completed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,16 +5,35 @@ import {Row, Col} from 'react-flexbox-grid';
 class Hero extends Component {
   render() {
     return (
-      <Row className="FullScreenHeight">
-        <Col xs={12} sm={12} md={6} className="Menu">
-          <div className="UserNameStyle">Melissa</div>
-          <br></br>
-          <div className="TinyDescStyle">International luxury model</div>
-        </Col>
-        <Col xs={12} sm={12} md={6}>
-          <div className="HeroImage"></div>
-        </Col>
-      </Row>
+      <div>
+        <Row className="FullScreenHeight">
+          <Col xs={12} sm={12} md={7} className="Menu">
+            <div>
+              <div className="UserNameStyle">Melissa</div>
+              <br></br>
+              <div className="TinyDescStyle">International luxury model</div>
+              <br></br>
+              <br></br>
+              <div className="CandyButts OrangeGradient">About me</div>
+              <br></br>
+              <br></br>
+              <div className="CandyButts PinkBlueGradient">Gallery</div>
+              <br></br>
+              <br></br>
+              <div className="CandyButts BlueVioletGradient">Schedule</div>
+              <br></br>
+              <br></br>
+              <div className="CandyButts AquaGradient">Rates</div>
+              <br></br>
+              <br></br>
+              <div className="CandyButts VioletOrangeGradient">Contact</div>
+            </div>
+          </Col>
+          <Col xs={0} sm={0} md={5}>
+            <div className="HeroImage"></div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
