672bedae139a8d79a4d6f501fd6295dc992053fa
Suresh Murali <m.suresh.fx@gmail.com>
2017-10-13 11:46:22 +0900

    Tiny Description under Customer Name

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,6 +8,8 @@ class Hero extends Component {
       <Row className="FullScreenHeight">
         <Col xs={12} sm={12} md={6} className="Menu">
           <div className="UserNameStyle">Melissa</div>
+          <br></br>
+          <div className="TinyDescStyle">International luxury model</div>
         </Col>
         <Col xs={12} sm={12} md={6}>
           <div className="HeroImage"></div>
